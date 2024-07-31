28439014733dd2f1b168a488ed4f4bf9b00d071b
Ben Lynch <96927431+benl17@users.noreply.github.com>
2024-07-31 15:57:05 -0500

    basic html changes

--- ToDo-List-SpringBoot/todolistfrontend/src/app/app-routing.module.ts
+++ ToDo-List-SpringBoot/todolistfrontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemListComponent } from './item-list/item-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'items', component: ItemListComponent},
+  {path: '', redirectTo: 'items', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ToDo-List-SpringBoot/todolistfrontend/src/app/app.module.ts
+++ ToDo-List-SpringBoot/todolistfrontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
